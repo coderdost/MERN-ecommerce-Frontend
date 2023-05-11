7fbfc0eb3914c201e01e06d622b71e580b22e9f3
Abhishek Rathore <rathore.abhishek87@gmail.com>
2023-05-12 01:15:30 +0530

    Part 26.1: Setting Proxy and Admin Panel order payment status

--- src/features/admin/components/AdminOrders.js
+++ src/features/admin/components/AdminOrders.js
@@ -30,8 +30,14 @@ function AdminOrders() {
     console.log('handleShow');
   };
 
-  const handleUpdate = (e, order) => {
+  const handleOrderStatus = (e, order) => {
     const updatedOrder = { ...order, status: e.target.value };
+    dispatch(updateOrderAsync(updatedOrder));
+    setEditableOrderId(-1);
+  };
+
+  const handleOrderPaymentStatus = (e, order) => {
+    const updatedOrder = { ...order, paymentStatus: e.target.value };
     dispatch(updateOrderAsync(updatedOrder));
     setEditableOrderId(-1);
   };
@@ -54,6 +60,8 @@ function AdminOrders() {
         return 'bg-yellow-200 text-yellow-600';
       case 'delivered':
         return 'bg-green-200 text-green-600';
+      case 'received':
+        return 'bg-green-200 text-green-600';
       case 'cancelled':
         return 'bg-red-200 text-red-600';
       default:
@@ -71,7 +79,7 @@ function AdminOrders() {
       <div className="bg-gray-100 flex items-center justify-center font-sans overflow-hidden">
         <div className="w-full">
           <div className="bg-white shadow-md rounded my-6">
-            <table className="min-w-max w-full table-auto">
+            <table className="w-full table-auto">
               <thead>
                 <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
                   <th
@@ -83,7 +91,7 @@ function AdminOrders() {
                       })
                     }
                   >
-                    Order# {' '}
+                    Order#{' '}
                     {sort._sort === 'id' &&
                       (sort._order === 'asc' ? (
                         <ArrowUpIcon className="w-4 h-4 inline"></ArrowUpIcon>
@@ -101,7 +109,7 @@ function AdminOrders() {
                       })
                     }
                   >
-                    Total Amount {' '}
+                    Total Amount{' '}
                     {sort._sort === 'totalAmount' &&
                       (sort._order === 'asc' ? (
                         <ArrowUpIcon className="w-4 h-4 inline"></ArrowUpIcon>
@@ -110,13 +118,19 @@ function AdminOrders() {
                       ))}
                   </th>
                   <th className="py-3 px-6 text-center">Shipping Address</th>
-                  <th className="py-3 px-6 text-center">Status</th>
+                  <th className="py-3 px-6 text-center">Order Status</th>
+                  <th className="py-3 px-6 text-center">Payment Method</th>
+                  <th className="py-3 px-6 text-center">Payment Status</th>
+
                   <th className="py-3 px-6 text-center">Actions</th>
                 </tr>
               </thead>
               <tbody className="text-gray-600 text-sm font-light">
                 {orders.map((order) => (
-                  <tr key={order.id} className="border-b border-gray-200 hover:bg-gray-100">
+                  <tr
+                    key={order.id}
+                    className="border-b border-gray-200 hover:bg-gray-100"
+                  >
                     <td className="py-3 px-6 text-left whitespace-nowrap">
                       <div className="flex items-center">
                         <div className="mr-2"></div>
@@ -125,7 +139,7 @@ function AdminOrders() {
                     </td>
                     <td className="py-3 px-6 text-left">
                       {order.items.map((item, index) => (
-                        <div key={index}  className="flex items-center">
+                        <div key={index} className="flex items-center">
                           <div className="mr-2">
                             <img
                               className="w-6 h-6 rounded-full"
@@ -159,7 +173,7 @@ function AdminOrders() {
                     </td>
                     <td className="py-3 px-6 text-center">
                       {order.id === editableOrderId ? (
-                        <select onChange={(e) => handleUpdate(e, order)}>
+                        <select onChange={(e) => handleOrderStatus(e, order)}>
                           <option value="pending">Pending</option>
                           <option value="dispatched">Dispatched</option>
                           <option value="delivered">Delivered</option>
@@ -175,6 +189,32 @@ function AdminOrders() {
                         </span>
                       )}
                     </td>
+
+                    <td className="py-3 px-6 text-center">
+                      <div className="flex items-center justify-center">
+                        {order.paymentMethod}
+                      </div>
+                    </td>
+
+                    <td className="py-3 px-6 text-center">
+                      {order.id === editableOrderId ? (
+                        <select
+                          onChange={(e) => handleOrderPaymentStatus(e, order)}
+                        >
+                          <option value="pending">Pending</option>
+                          <option value="received">Received</option>
+                        </select>
+                      ) : (
+                        <span
+                          className={`${chooseColor(
+                            order.paymentStatus
+                          )} py-1 px-3 rounded-full text-xs`}
+                        >
+                          {order.paymentStatus}
+                        </span>
+                      )}
+                    </td>
+
                     <td className="py-3 px-6 text-center">
                       <div className="flex item-center justify-center">
                         <div className="w-6 mr-4 transform hover:text-purple-500 hover:scale-120">
